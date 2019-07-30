83dd88d7d1840e93cf4049b3585d2d78af220971
DGD18JonathanGuite <jonathan_g@nid.edu>
2019-07-30 23:24:49 +0530

    v.05

--- Assets/JontyScripts/ChargeInputManager.cs
+++ Assets/JontyScripts/ChargeInputManager.cs
@@ -6,7 +6,16 @@ public class ChargeInputManager : MonoBehaviour
 {
     private void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.W) && gameObject.GetComponent<ChargeMovement>().WayPointID == 0 && GameObject.Find("RotatingPanel").GetComponent<PanelTestRotation>().Rotating == false)
+        //if (Input.GetKeyDown(KeyCode.W) && gameObject.GetComponent<ChargeMovement>().WayPointID == 0 && GameObject.Find("RotatingPanel").GetComponent<PanelTestRotation>().Rotating == false)
+        //{
+        //    gameObject.GetComponent<ChargeChangeWayPoints>().PickWayPointSet();
+        //    GameObject.Find("RotatingPanel").GetComponent<PanelTestRotation>().RotatePanel();
+        //}
+    }
+
+    public void RequestRotation()
+    {
+        if (gameObject.GetComponent<ChargeMovement>().WayPointID == 0 && GameObject.Find("RotatingPanel").GetComponent<PanelTestRotation>().Rotating == false)
         {
             gameObject.GetComponent<ChargeChangeWayPoints>().PickWayPointSet();
             GameObject.Find("RotatingPanel").GetComponent<PanelTestRotation>().RotatePanel();
--- Assets/JontyScripts/RotatingPower.cs
+++ Assets/JontyScripts/RotatingPower.cs
@@ -2,38 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotatorPower : MonoBehaviour
+public class RotatingPower : MonoBehaviour
 {
     public Power RotatorScript;
     public float RotatorPower1;
-    public float RotatorCharged=3;
+    public float RotatorCharged=0.8f;
     public bool CollisionOn;
     public Material RotatorOff, RotatorOn;
     public float ChargeSpeed = 3;
 
+    public GameObject Charge;
 
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.GetComponent<MeshRenderer>().material = RotatorOff;
+        //gameObject.GetComponent<MeshRenderer>().material = RotatorOff;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (CollisionOn == true)
         {
-
+            Debug.Log("Shouldbecharging");
             if (RotatorScript.ButtonHeldDown == true)
             {
-
                 RotatorPower1 += Time.deltaTime * ChargeSpeed;
             }
         }
 
         if (RotatorPower1 > RotatorCharged)
-            Debug.Log("TurnRotator");
-
+        {
+            Charge.GetComponent<ChargeInputManager>().RequestRotation();
+            RotatorPower1 = 0;
+        }
     }
 
 
@@ -43,14 +45,13 @@ public class RotatorPower : MonoBehaviour
         {
             CollisionOn = true;
         }
-
     }
 
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "spark")
         {
-            Debug.Log("ON");
+            //Debug.Log("ON");
             CollisionOn = true;
         }
 
@@ -60,7 +61,7 @@ public class RotatorPower : MonoBehaviour
     {
         if (other.gameObject.tag == "spark")
         {
-            Debug.Log("OFF");
+            //Debug.Log("OFF");
             CollisionOn = false;
         }
 
