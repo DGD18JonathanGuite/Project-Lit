98a37bbe5478e2edab6f4fdb97d85a9b66498411
Xenocrone <44226938+Xenocrone@users.noreply.github.com>
2019-07-31 17:21:35 +0530

    Betav2

---
+++ Assets/Spacebar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spacebar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            gameObject.GetComponent<>.
+            //gameObject.renderer.material.color = Color.white;
+        }
+    }
+}
---
+++ Assets/WordScreen.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WordScreen : MonoBehaviour
+{
+    public Material WordOff, WordOn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<MeshRenderer>().material = WordOff;
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.FindGameObjectsWithTag("Unlit").Length == 0)
+        {
+            gameObject.GetComponent<MeshRenderer>().material = WordOn;
+            Invoke("Next", 2f);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        }
+    }
+
+    void NextLevel()
+    {
+        
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        
+
+       
+    }
+}
